730dcbc8636e7903b8c18287ee08260978a3bb53
Vidhi Sikarwar <vidhisikarwar57@gmail.com>
2026-09-25 14:11:36 +0530

    Configure Django for Vercel deployment

--- StudentHub/StudentHub/settings.py
+++ StudentHub/StudentHub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#%hc!v@k%xbc2ee^02v%i$&y^k86)*a_9cd!79(m77hct4k^ki'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
